ed64e92d4e8ae8ae3e421c2ae1ca87ab73a7cf87
peregrinescode <ross.warren@pm.me>
2026-03-03 22:28:28 +0100

    measure air quality

--- weather_station_MEASURE.py
+++ weather_station_MEASURE.py
@@ -1,3 +1,4 @@
+import sds011
 import bme280_sensor
 import mysql.connector as database
 from time import sleep
@@ -10,15 +11,16 @@ connection = database.connect(
 
 cursor = connection.cursor()
 
-def add_data(ambient_temp, air_quality, pressure, humidity):
+def add_data(ambient_temp, air_quality_pm2, air_quality_pm10, pressure, humidity):
     try:
-        statement = "INSERT INTO WEATHER_MEASUREMENT (AMBIENT_TEMPERATURE, AIR_QUALITY, AIR_PRESSURE, HUMIDITY) VALUES (%s, %s, %s, %s)"
-        data = (ambient_temp, air_quality, pressure, humidity)
+        statement = "INSERT INTO WEATHER_MEASUREMENT (AMBIENT_TEMPERATURE, AIR_QUALITY_PM2, AIR_QUALITY_PM10, AIR_PRESSURE, HUMIDITY) VALUES (%s, %s, %s, %s, %s)"
+        data = (ambient_temp, air_quality_pm2, air_quality_pm10, pressure, humidity)
         cursor.execute(statement, data)
         connection.commit()
-        print("Successfully added entry to database")
+        # print("Successfully added entry to database")
     except database.Error as e:
-        print(f"Error adding entry to database: {e}")
+        # print(f"Error adding entry to database: {e}")
+        pass
 
 #def get_data():
 #    try:
@@ -30,9 +32,20 @@ def add_data(ambient_temp, air_quality, pressure, humidity):
 #    except database.Error as e:
 #      print(f"Error retrieving entry from database: {e}")
 
-while True:
-    humidity, pressure, ambient_temp = bme280_sensor.read_all()
-    add_data(ambient_temp, "0", pressure, humidity)
-    sleep(5)
+# Read SDS011 sensor
+sds011.cmd_set_sleep(0)		# turn on sensor
+sleep(15)			# give it 10 seconds to power on
+values = sds011.cmd_query_data()
+sds011.cmd_set_sleep(1)
+
+
+# Read BME280 sensor
+humidity, pressure, ambient_temp = bme280_sensor.read_all()
+
+# Print data
+# print("Humidity: ", humidity, ", Pressure: ", pressure, ", Temp: ", ambient_temp, ", PM2.5: ", values[0], ", PM10: ", values[1])
+
+# Save data to DB
+add_data(ambient_temp, values[0], values[1], pressure, humidity)
 
 connection.close()
